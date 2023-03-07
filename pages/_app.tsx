e970eb9e8797c84ca402fe7070a5110cd7dcabb2
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2023-03-07 13:12:25 +0100

    Enable React Strict Mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type {AppProps} from "next/app";
+import {StrictMode} from "react";
 
 export default function App({Component, pageProps}: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<StrictMode>
+			<Component {...pageProps} />
+		</StrictMode>
+	);
 }
