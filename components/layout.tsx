a65f42266c95b4bd60ad338b75bd405ad0c13d9d
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2023-03-12 15:37:52 +0100

    Add font to <html> tag
    That ensures that every element on the page gets the same font.

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,18 +1,9 @@
 import {PropsWithChildren} from "react";
 
-// Next.js downloads the fonts and saves them locally. No requests are sent to Google by the browser.
-// https://nextjs.org/docs/basic-features/font-optimization
-import {Inter} from "next/font/google";
-
-const inter = Inter({
-	subsets: ["latin"],
-	variable: "--font-inter",
-});
-
 export default function Layout({children}: PropsWithChildren) {
 	return (
-		<div className={`${inter.variable} font-sans`}>
+		<>
 			{children}
-		</div>
+		</>
 	);
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,30 @@ import type {AppProps} from "next/app";
 import {StrictMode} from "react";
 import Layout from "@/components/layout";
 
+// Next.js downloads the fonts and saves them locally. No requests are sent to Google by the browser.
+// https://nextjs.org/docs/basic-features/font-optimization
+import {Inter} from "next/font/google";
+
+const inter = Inter({
+	subsets: ["latin"],
+	preload: true
+});
+
 export default function App({Component, pageProps}: AppProps) {
 	return (
 		<StrictMode>
 			<Layout>
+				{/*
+					Found no better way to do connect next/font with Tailwind as of Next.js 13.0. See explanation on
+					https://levelup.gitconnected.com/how-to-make-next-js-13s-optimized-fonts-work-with-tailwind-css-c3c5e57d38aa.
+				*/}
+				<style jsx global>
+					{`
+						:root {
+							--inter-font: ${inter.style.fontFamily};
+						}
+					`}
+				</style>
 				<Component {...pageProps} />
 			</Layout>
 		</StrictMode>
