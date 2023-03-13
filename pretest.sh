c876ffbbf64fe0c1b30b58a5ee4ff4697923ef0c
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2023-03-13 13:01:48 +0100

    Add pretest script

---
+++ pretest.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+set -Eeuo pipefail
+
+# Linux/macOS compatibility shim, see https://unix.stackexchange.com/a/84980.
+TEMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t "jdkcomparison")
+PID=
+
+cleanup() {
+	if [ -n "$PID" ]; then
+		kill -TERM "$PID"
+    fi
+	exit
+}
+
+trap cleanup EXIT SIGHUP SIGINT SIGQUIT SIGABRT
+
+rm -rf .next
+npm run build
+
+npm run start > "$TEMP_DIR/nextjs-e2e.txt" 2>&1 &
+PID=$!
+npm run e2e:headless
