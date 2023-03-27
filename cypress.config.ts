cd0c53c52feff02c6e0a16f25f5ad23f7c7cfdda
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2023-03-27 14:38:52 +0200

    Reformat Cypress configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from "cypress";
+import {defineConfig} from "cypress";
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
-  },
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	component: {
+		devServer: {
+			framework: "next",
+			bundler: "webpack",
+		},
+	},
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
 });
