df8cd147ba3a32ee3527ac20263a4fe415814b91
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2023-04-13 15:49:12 +0200

    Add license headers

--- data/oracle.ts
+++ data/oracle.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. To view a copy of
+ * this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or send a letter to Creative Commons, PO Box 1866,
+ * Mountain View, CA 94042, USA.
+ */
 import { Present, Vendor } from "@/src/vendorDataTypes";
 
 export const data: Vendor = {
