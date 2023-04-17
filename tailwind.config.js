ff640543c6443c8d76e56a3a36a59ab9b56357a8
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2023-04-17 13:09:33 +0200

    Ensure that Tailwind sees mdx files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-	content: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+	content: ["./components/**/*.{js,ts,jsx,tsx,mdx}", "./pages/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
 		extend: {
 			fontFamily: {
