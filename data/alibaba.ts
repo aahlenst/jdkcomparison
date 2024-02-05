78f411a0b8187351fd8f77d3f87f17f76c5da56f
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2024-02-05 18:46:49 +0100

    Add Dragonwell 21 SE

--- data/alibaba.ts
+++ data/alibaba.ts
@@ -229,10 +229,7 @@ export const data: Vendor = {
 					s390x: { present: Present.NO },
 					apk: { present: Present.NO },
 					deb: { present: Present.NO },
-					rpm: {
-						present: Present.PARTIALLY,
-						footnote: "30c5b423-f759-49ad-bc4c-2f6cdf9cb8fe",
-					},
+					rpm: { present: Present.NO },
 					containerImages: { present: Present.YES },
 				},
 				windows: {
@@ -408,10 +405,7 @@ export const data: Vendor = {
 					s390x: { present: Present.NO },
 					apk: { present: Present.NO },
 					deb: { present: Present.NO },
-					rpm: {
-						present: Present.PARTIALLY,
-						footnote: "30c5b423-f759-49ad-bc4c-2f6cdf9cb8fe",
-					},
+					rpm: { present: Present.NO },
 					containerImages: { present: Present.YES },
 				},
 				windows: {
@@ -455,6 +449,90 @@ export const data: Vendor = {
 				remarks: { text: "none" },
 			},
 		},
+		{
+			id: "dragonwell-21-se",
+			information: {
+				name: "Dragonwell 21 SE",
+				version: 21,
+				downloadSite: "https://dragonwell-jdk.io/",
+			},
+			features: {
+				virtualMachine: { text: "HotSpot" },
+				classLibraries: { text: "OpenJDK" },
+				javaFX: { present: Present.NO },
+				flightRecorder: { present: Present.YES },
+				javaWS: { present: Present.NO },
+				gc: {
+					cms: { present: Present.NO },
+					epsilon: { present: Present.YES },
+					g1: { present: Present.YES },
+					parallel: { present: Present.YES },
+					serial: { present: Present.YES },
+					shenandoah: { present: Present.YES },
+					z: { present: Present.YES },
+					custom: { text: "none" },
+				},
+				mac: {
+					x64: { present: Present.NO },
+					aarch64: { present: Present.NO },
+					installers: { present: Present.NO },
+				},
+				linux: {
+					x32: { present: Present.NO },
+					x64: { present: Present.YES },
+					x64Musl: { present: Present.YES },
+					aarch64: { present: Present.YES },
+					aarch64Musl: { present: Present.NO },
+					aarch32: { present: Present.NO },
+					ppc64: { present: Present.NO },
+					riscv64: { present: Present.NO },
+					s390x: { present: Present.NO },
+					apk: { present: Present.NO },
+					deb: { present: Present.NO },
+					rpm: { present: Present.NO },
+					containerImages: { present: Present.YES },
+				},
+				windows: {
+					x32: { present: Present.NO },
+					x64: { present: Present.YES },
+					aarch64: { present: Present.NO },
+					installers: { present: Present.NO },
+					containerImages: { present: Present.NO },
+				},
+				otherPlatforms: {
+					aixPPC: { present: Present.NO },
+					solarisSPARC: { present: Present.NO },
+					solarisx64: { present: Present.NO },
+				},
+				aqavit: { present: Present.UNKNOWN },
+				tck: { present: Present.NO },
+				editions: { text: "none" },
+				customisations: {
+					text: "few",
+					footnote: "657156b2-7798-458b-b93a-d7b1b207e317",
+				},
+				notableFeatures: { text: "none" },
+				license: { text: "GPLv2+CE" },
+				freeInDevelopment: { present: Present.YES },
+				freeInProduction: { present: Present.YES },
+				sbom: {
+					present: Present.YES,
+					footnote: "3c3737e8-faa9-4954-9fa7-62e130a15332",
+				},
+				paidSupport: { present: Present.NO },
+				eolDate: {
+					text: "n/a",
+					footnote: "5377b31e-ff3a-4a7b-a03b-890aebb0f971",
+				},
+				releaseSchedule: {
+					text: "Custom",
+					footnote: "a57fac2d-6fbd-4e2d-a5cf-4352b455ec2e",
+				},
+				releaseDelay: { text: "n/a" },
+				updateTypes: { text: "no/free" },
+				remarks: { text: "none" },
+			},
+		},
 	],
 	footnotes: [
 		{
@@ -469,7 +547,8 @@ export const data: Vendor = {
 				"Patches applied on top of OpenJDK are listed in the release notes of the each Dragonwell version: " +
 				"[8](https://github.com/dragonwell-project/dragonwell8/wiki/Alibaba-Dragonwell8-Standard-Edition-Release-Notes), " +
 				"[11](https://github.com/dragonwell-project/dragonwell11/wiki/Alibaba-Dragonwell-11-Standard-Edition-Release-Notes), " +
-				"[17](https://github.com/dragonwell-project/dragonwell17/wiki/Alibaba-Dragonwell-17-Standard-Edition-Release-Notes).",
+				"[17](https://github.com/dragonwell-project/dragonwell17/wiki/Alibaba-Dragonwell-17-Standard-Edition-Release-Notes), " +
+				"[21](https://github.com/dragonwell-project/dragonwell21/wiki/Alibaba-Dragonwell-21-Standard-Edition-Release-Notes).",
 		},
 		{
 			id: "370c0aa7-27c9-445a-b4f7-8a530d3d335f",
@@ -484,7 +563,8 @@ export const data: Vendor = {
 				"For details, see the Release Roadmap for Alibaba Dragonwell " +
 				"[8](https://github.com/dragonwell-project/dragonwell8/wiki/Alibaba-Dragonwell-Support#release-roadmap), " +
 				"[11](https://github.com/dragonwell-project/dragonwell11/wiki/Alibaba-Dragonwell-Support#release-roadmap), " +
-				"[17](https://github.com/dragonwell-project/dragonwell17/wiki/Alibaba-Dragonwell-Support#release-roadmap).",
+				"[17](https://github.com/dragonwell-project/dragonwell17/wiki/Alibaba-Dragonwell-Support#release-roadmap), " +
+				"[21](https://github.com/dragonwell-project/dragonwell21/wiki/Alibaba-Dragonwell-Support#release-roadmap).",
 		},
 		{
 			id: "a57fac2d-6fbd-4e2d-a5cf-4352b455ec2e",
@@ -504,7 +584,8 @@ export const data: Vendor = {
 				"SBOMs can be found on the GitHub Releases page of the respective Dragonwell version: " +
 				"[8](https://github.com/dragonwell-project/dragonwell8/releases), " +
 				"[11](https://github.com/dragonwell-project/dragonwell11/releases), " +
-				"[17](https://github.com/dragonwell-project/dragonwell17/releases).",
+				"[17](https://github.com/dragonwell-project/dragonwell17/releases), " +
+				"[21](https://github.com/dragonwell-project/dragonwell21/releases).",
 		},
 		{
 			id: "8f22b6d3-9af1-426e-9fb5-5846c8e91791",
