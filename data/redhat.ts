fb89bf52192afcff70fa53ff6d479bd5e994ac6b
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2024-02-06 14:52:25 +0100

    Add Red Hat OpenJDK 21

--- data/redhat.ts
+++ data/redhat.ts
@@ -300,6 +300,102 @@ export const data: Vendor = {
 				remarks: { text: "none" },
 			},
 		},
+		{
+			id: "rh-build-of-openjdk-21",
+			information: {
+				name: "Build of OpenJDK 21",
+				version: 21,
+				downloadSite: "https://developers.redhat.com/products/openjdk/download",
+			},
+			features: {
+				virtualMachine: { text: "HotSpot" },
+				classLibraries: { text: "OpenJDK" },
+				javaFX: { present: Present.NO },
+				flightRecorder: { present: Present.YES },
+				javaWS: { present: Present.NO },
+				gc: {
+					cms: { present: Present.NO },
+					epsilon: { present: Present.YES },
+					g1: { present: Present.YES },
+					parallel: { present: Present.YES },
+					serial: { present: Present.YES },
+					shenandoah: {
+						present: Present.PARTIALLY,
+						footnote: "5cc2a923-cca5-4984-93bb-861cb304b64b",
+					},
+					z: {
+						present: Present.PARTIALLY,
+						footnote: "17cf5639-7d55-4800-8c17-62950b37420c",
+					},
+					custom: { text: "none" },
+				},
+				mac: {
+					x64: { present: Present.NO },
+					aarch64: { present: Present.NO },
+					installers: { present: Present.NO },
+				},
+				linux: {
+					x32: { present: Present.NO },
+					x64: { present: Present.YES },
+					x64Musl: { present: Present.NO },
+					aarch64: {
+						present: Present.PARTIALLY,
+						footnote: "f4cfc6a3-190c-4266-96a5-76947275f457",
+					},
+					aarch64Musl: { present: Present.NO },
+					aarch32: { present: Present.NO },
+					ppc64: {
+						present: Present.PARTIALLY,
+						footnote: "f4cfc6a3-190c-4266-96a5-76947275f457",
+					},
+					riscv64: { present: Present.NO },
+					s390x: {
+						present: Present.PARTIALLY,
+						footnote: "f4cfc6a3-190c-4266-96a5-76947275f457",
+					},
+					apk: { present: Present.NO },
+					deb: { present: Present.NO },
+					rpm: {
+						present: Present.PARTIALLY,
+						footnote: "f4cfc6a3-190c-4266-96a5-76947275f457",
+					},
+					containerImages: { present: Present.YES },
+				},
+				windows: {
+					x32: { present: Present.NO },
+					x64: { present: Present.YES },
+					aarch64: { present: Present.NO },
+					installers: { present: Present.YES },
+					containerImages: { present: Present.NO },
+				},
+				otherPlatforms: {
+					aixPPC: { present: Present.NO },
+					solarisSPARC: { present: Present.NO },
+					solarisx64: { present: Present.NO },
+				},
+				aqavit: { present: Present.YES },
+				tck: { present: Present.YES },
+				editions: { text: "JRE" },
+				customisations: { text: "few", footnote: "b40e7cb3-f459-4331-b9fb-bd36c6740b5a" },
+				notableFeatures: { text: "none" },
+				license: { text: "GPLv2+CE" },
+				freeInDevelopment: { present: Present.YES },
+				freeInProduction: { present: Present.YES },
+				sbom: { present: Present.NO },
+				paidSupport: {
+					present: Present.YES,
+					footnote: "16864047-89f2-4d27-af78-2233b8b10960",
+				},
+				eolDate: {
+					text: "2029-12",
+					footnote: "2ce6dbb8-bba6-4012-99f1-970796ba79f9",
+				},
+				releaseSchedule: { text: "OpenJDK" },
+				releaseDelay: { text: "0-3 days" },
+				updateTypes: { text: "no/free" },
+				remarks: { text: "none" },
+			},
+		},
 	],
 	footnotes: [
 		{
@@ -348,7 +444,8 @@ export const data: Vendor = {
 				"respective version: " +
 				"[8](https://access.redhat.com/documentation/en-us/openjdk/8), " +
 				"[11](https://access.redhat.com/documentation/en-us/openjdk/11), " +
-				"[17](https://access.redhat.com/documentation/en-us/openjdk/17).",
+				"[17](https://access.redhat.com/documentation/en-us/openjdk/17), " +
+				"[21](https://access.redhat.com/documentation/en-us/openjdk/21).",
 		},
 	],
 };
