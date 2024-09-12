9c5921e5c3bcaa5ac92463d4e8fb246e2ff4d21e
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2024-09-12 14:02:25 +0200

    Add missing import

--- src/sorting.test.ts
+++ src/sorting.test.ts
@@ -14,7 +14,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-import { describe, expect, test } from "@jest/globals";
+import { beforeEach, describe, expect, test } from "@jest/globals";
 import { Model } from "@/src/modelTypes";
 import { extractComparisonData } from "@/src/comparison";
 import {
