4d89ac7cd43af8e308f4714c85a75ef955ba841a
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2025-07-06 21:28:45 +0200

    Simplify code

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,9 +11,7 @@ export default defineConfig({
 	retries: process.env.CI ? 2 : 0,
 	reporter: process.env.CI ? "github" : "list",
 	use: {
-		baseURL: process.env.PLAYWRIGHT_BASE_URL
-			? process.env.PLAYWRIGHT_BASE_URL
-			: "http://127.0.0.1:3000",
+		baseURL: process.env.PLAYWRIGHT_BASE_URL ?? "http://127.0.0.1:3000",
 		trace: "on-first-retry",
 	},
 
