57667c05c09d1cc288c89e4aae95fbb84fc6754e
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2025-10-22 15:53:17 +0200

    Update Next.js to v16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,22 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
-import { FlatCompat } from "@eslint/eslintrc";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-const compat = new FlatCompat({
-	baseDirectory: import.meta.dirname,
-});
-
-export default defineConfig([
-	...compat.extends("next/core-web-vitals", "next/typescript"),
-	{
-		ignores: [
-			".next",
-			"jest.config.js",
-			"next-env.d.ts",
-			"next.config.mjs",
-			"node_modules",
-			"out",
-			"postcss.config.mjs",
-			"tailwind.config.mjs",
-		],
-	},
+const eslintConfig = defineConfig([
+	nextVitals,
+	nextTs,
+	globalIgnores([
+		".next",
+		"jest.config.js",
+		"next-env.d.ts",
+		"next.config.mjs",
+		"node_modules",
+		"out",
+		"postcss.config.mjs",
+		"tailwind.config.mjs",
+	]),
 	{
 		languageOptions: {
 			parserOptions: {
@@ -32,8 +28,11 @@ export default defineConfig([
 		},
 		rules: {
 			"@typescript-eslint/no-deprecated": "error",
+			"react-hooks/refs": "off", // Off because of https://github.com/facebook/react/issues/34775
 		},
 	},
 	js.configs.recommended,
 	tseslint.configs.recommendedTypeChecked,
 ]);
+
+export default eslintConfig;
