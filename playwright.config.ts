673a3f277f52fd74b930336f075cd9759eff4b2f
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2025-10-22 15:03:32 +0200

    Make number of Playwright workers configurable

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,19 @@
 import { defineConfig, devices } from "@playwright/test";
 
+const playwrightWorkers = (): string | number | undefined => {
+	if (!("PLAYWRIGHT_WORKERS" in process.env) || process.env.PLAYWRIGHT_WORKERS === undefined) {
+		console.log("Returning undefined");
+		return undefined;
+	}
+
+	const num_workers = +process.env.PLAYWRIGHT_WORKERS;
+	if (!Number.isNaN(num_workers)) {
+		return num_workers;
+	}
+
+	return process.env.PLAYWRIGHT_WORKERS;
+};
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -10,6 +24,7 @@ export default defineConfig({
 	timeout: 10000,
 	retries: process.env.CI ? 2 : 0,
 	reporter: process.env.CI ? "github" : "list",
+	workers: playwrightWorkers(),
 	use: {
 		baseURL: process.env.PLAYWRIGHT_BASE_URL ?? "http://127.0.0.1:3000",
 		trace: "on-first-retry",
