8263895737e00a0e041405904fe9240d2bd76f4f
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2026-03-17 15:51:41 +0100

    Add Oracle JDK 26 and OpenJDK 26

--- tests/acceptance/desktop/index.spec.ts
+++ tests/acceptance/desktop/index.spec.ts
@@ -290,7 +290,7 @@ test.describe("JDK Comparison", () => {
 	test("all options of filter Versions are displayed", async ({ page }) => {
 		await page.getByRole("button", { name: "Versions", exact: true }).click();
 
-		const options = ["8", "11", "17", "21", "24", "25"];
+		const options = ["8", "11", "17", "21", "24", "25", "26"];
 		const form = page.getByRole("form", { name: "Selectable Versions" });
 		await expect(form.getByRole("checkbox")).toHaveCount(options.length);
 		for (let i = 0; i < options.length; i++) {
